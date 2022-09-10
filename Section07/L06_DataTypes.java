13e47d5e95ced839177159d9506cbf58dd6c6685
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-09-09 21:22:08 -0700

    Add files via upload

---
+++ Section07/L06_DataTypes.java
@@ -0,0 +1,28 @@
+package Section07;
+
+public class L06_DataTypes {
+	
+	public static void main(String[] args) {
+		
+		String dogBreed = "poodle";
+		
+		int number = 10;
+		
+		boolean flag = true;
+		
+		char letter = 'a';
+		
+		float example = 1.123f;
+		
+		double doubExample = 1.234;
+		
+		
+		System.out.println(dogBreed);
+		System.out.println(number);
+		System.out.println(flag);
+		System.out.println(letter);
+		System.out.println(example);
+		System.out.println(doubExample);
+	}
+
+}
